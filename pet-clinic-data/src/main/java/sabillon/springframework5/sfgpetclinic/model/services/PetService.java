1078d79c211e19fc97c9994fdb4141b46d9e6db1
Mike Sabillon <mikesabillon2014@gmail.com>
2018-11-25 15:37:53 +0100

    Added Services to data module

--- pet-clinic-data/src/main/java/sabillon/springframework5/sfgpetclinic/model/services/PetService.java
+++ pet-clinic-data/src/main/java/sabillon/springframework5/sfgpetclinic/model/services/PetService.java
@@ -1,4 +1,35 @@
 package sabillon.springframework5.sfgpetclinic.model.services;
 
+import sabillon.springframework5.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+/**
+ * The interface Pet service.
+ */
 public interface PetService {
+
+    /**
+     * Find by id pet.
+     *
+     * @param id the id
+     * @return the pet
+     */
+    Pet findById(Long id);
+
+    /**
+     * Save pet.
+     *
+     * @param pet the pet
+     * @return the pet
+     */
+    Pet Save(Pet pet);
+
+    /**
+     * Find all set.
+     *
+     * @return the set
+     */
+    Set<Pet> findAll();
+
 }
--- pet-clinic-data/src/main/java/sabillon/springframework5/sfgpetclinic/model/services/VetService.java
+++ pet-clinic-data/src/main/java/sabillon/springframework5/sfgpetclinic/model/services/VetService.java
@@ -1,4 +1,35 @@
 package sabillon.springframework5.sfgpetclinic.model.services;
 
+import sabillon.springframework5.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+/**
+ * The interface Vet service.
+ */
 public interface VetService {
+
+    /**
+     * Find by id vet.
+     *
+     * @param id the id
+     * @return the vet
+     */
+    Vet findById(Long id);
+
+    /**
+     * Save vet.
+     *
+     * @param vet the vet
+     * @return the vet
+     */
+    Vet save(Vet vet);
+
+    /**
+     * Find all set.
+     *
+     * @return the set
+     */
+    Set<Vet> findAll();
+
 }
